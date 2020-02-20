fecc0b3cf9e587aed4b8fa261b543807a9291def
ninoloid <muhammadsatriaadiputra@gmail.com>
2020-02-20 22:45:57 +0700

    styling done, without result page

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import LandingPage from '../views/landingPage.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'landingPage',
+    component: LandingPage
   },
   {
     path: '/inGame/:id',
@@ -17,14 +17,6 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/inGame.vue')
-  },
-  {
-    path: '/landingPage',
-    name: 'landingPage',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/landingPage.vue')
   }
 ]
 
